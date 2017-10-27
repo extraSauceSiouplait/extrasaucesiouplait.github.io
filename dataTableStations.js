719638e68cbac22c0c1c12152aec96b5093fa7df
Mathieu-2 Belanger <mbelanger.poly@gmail.com>
2017-10-27 19:41:18 -0400

    yay

--- dataTableStations.js
+++ dataTableStations.js
@@ -3,21 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var = stations
+/*
 $(document).ready(function() {
-    $('#tableStations').DataTable( 
-        $.ajax({
-            url: "https://secure.bixi.com/data/stations.json",
-            dataType: "json",
-            type: "POST",
-            success: function(data) {
-            response($.map(data.stations, function(item) {
-                return {
-                    label: item.s,
-                    value: item.id
-                };
-            }));
-            }
+    $.ajax({
+        url: "https://secure.bixi.com/data/stations.json",
+        dataType: "json",
+        type: "POST",
+        success: function(data) {
+        response($.map(data.stations, function(item) {
+            return {
+                label: item.s,
+                value: item.id
+            };
+        }));
+        }
         }),
     );
+});
+*/
+
+$("autocomplete").autocomplete({
+    source: function(event, ui) {
+        $(".intersectionRues .nomStation").text(ui.item.label);
+        $ajax({
+            url: 'https://secure.bixi.com/data/stations.json',
+            dataType = 'json',
+            success: function(data){
+                var stations = data.stations.find(function(item){
+                    return item.s;
+                }); 
+            }
+        })
+    }
 });
