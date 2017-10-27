bd4290332247555f66fdd27d1b64cfa2ee30c26a
Mathieu-2 Belanger <mbelanger.poly@gmail.com>
2017-10-27 19:48:08 -0400

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var = stations
+var stations;
 /*
 $(document).ready(function() {
     $.ajax({
